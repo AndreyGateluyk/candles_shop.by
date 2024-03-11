1a6a6de429eb535115189b0130357d3a938e11ae
Andrey Gateluyk <gatelyuk97@mail.ru>
2024-03-12 00:28:28 +0300

    feat: add render card with js

---
+++ candles_shop/menu/data/products.js
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    id: 1,
+    image: "../menu/image/candles/bubble.jpg",
+    name: "Формовая свеча",
+    text: "Натуральный соевый воск, хлопковый фитиль",
+    price: 9
+  },
+  {
+    id: 2,
+    image: "../menu/image/candles/women.jpg",
+    name: "Свеча в форме женского тела",
+    text: "Натуральный соевый воск, хлопковый фитиль",
+    price: 10
+  },
+  {
+    id: 3,
+    image: "../menu/image/candles/men.jpg",
+    name: "Свеча в форме мужского тела",
+    text: "Натуральный соевый воск, хлопковый фитиль",
+    price: 10
+  },
+  {
+    id: 4,
+    image: "../menu/image/candles/container.jpg",
+    name: "Контейнерная свеча",
+    text: "Натуральный соевый воск, деревянный фитиль",
+    price: 12
+  },
+  {
+    id: 5,
+    image: "../menu/image/candles/home.jpg",
+    name: "Свеча 'Семейный очаг'",
+    text: "Пчелиный воск, натуральная вощина",
+    price: 8
+  },
+  {
+    id: 6,
+    image: "../menu/image/candles/sfera.jpg",
+    name: "Формовая свеча 'Клубок ниток'",
+    text: "Натуральный соевый воск, хлопковый фитиль",
+    price: 9
+  }
+]
--- candles_shop/menu/js/index.js
+++ candles_shop/menu/js/index.js
@@ -1,84 +1,32 @@
-const offerBtn = document.querySelectorAll('.menu__offer-btn');
-const gridItems = document.querySelectorAll('.menu__item')
-const offer = document.querySelector('.menu__offer')
-const loadBtn = document.querySelector('.load-btn')
-const menuCard = document.querySelectorAll('.menu__card')
+import { products } from "../data/products.js";
 
-//function show menu section
-offer.addEventListener("click", showMenuSection);
+const menuGrid = document.querySelector(".menu__grid");
 
-function showMenuSection(e) {
-  if(e.target.className === 'menu__offer-btn') {
-    for( let i = 0; i < offerBtn.length; i += 1) {
-      offerBtn[i].className = 'menu__offer-btn';
-    }
-    e.target.classList.add('menu__offer-btn_active');
-  }
-  for(let i = 0; i < gridItems.length; i+= 1) {
-    if(`${e.target.id}-block` === gridItems[i].id) {
-      gridItems[i].className = gridItems[i].className.replace('hidden', 'active');
-      let targetBlock = gridItems[i].childNodes;
-      let targetBlockItems = [];
-      targetBlock.forEach((item, index) => {
-        if(index % 2 === 1) {
-          targetBlockItems.push(item);
-        }
-      })
-      if(window.innerWidth <= 768) {
-        if(targetBlockItems.length >= 4) {
-          loadBtn.classList.remove('hidden');
-        }
-        if(targetBlockItems.length < 5) {
-          loadBtn.classList.add('hidden');
-        }
-        
-        targetBlockItems.forEach((item, index) => {
-          if(index > 3) {
-            item.classList.add('hidden');
-          }
-        })
-      }
-    }
-      else {
-        gridItems[i].classList.replace('active', 'hidden');
-      }
-    }
+function renderMenuCards() {
+  products.forEach(card => {
+    const { id, image, name, text, price} = card
+    const cardItem =
+      `
+        <div class="menu__card" product-id="${id}">
+          <div class="menu__card-img">
+            <img src="${image}" alt="">
+          </div>
+          <div class="menu__card-description">
+            <div class="menu__card-name">
+              ${name}
+            </div>
+            <div class="menu__card-text">
+              ${text}
+            </div>
+            <div class="menu__card-price">
+              ${price} BYN
+            </div>
+          </div>
+        </div>
+      `
+    menuGrid.insertAdjacentHTML('beforeend', cardItem)
+  });
 }
-//window size tracking
-window.addEventListener('resize', EventResize);
 
-function EventResize() {
-  let activeItems = document.querySelector('.active').childNodes;
-  let activeItemsArr = [];
-  activeItems.forEach((item, index) => {
-    if(index % 2 === 1) {
-      activeItemsArr.push(item);
-    }
-  })
-  if(document.body.clientWidth <= 768) {
-    loadBtn.classList.remove('hidden');
-    activeItemsArr.forEach((item, index) => {
-      if(index > 3) {
-        item.classList.add('hidden');
-      }
-    })
-    if(activeItemsArr.length < 5) {
-      loadBtn.classList.add('hidden');
-    }
-  }
-  if(document.body.clientWidth > 768 ) {
-    loadBtn.classList.add('hidden');
-    activeItemsArr.forEach((item) => {
-      item.classList.remove('hidden');
-    })
-  }
-}
-EventResize()
+renderMenuCards()
 
-//show hidden menu items
-loadBtn.addEventListener('click', () => {
-  menuCard.forEach((item) => {
-    item.classList.remove('hidden');
-  })
-  loadBtn.classList.add('hidden');
-})
